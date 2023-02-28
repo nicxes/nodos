ee064bbf87d49b9cb44b43eb3d80e2d736de7130
Nicxes <nicxesh@gmail.com>
2023-02-28 16:02:19 -0300

    push(terms/privacy): Added terms and privacy content text

--- pages/terminos.vue
+++ pages/terminos.vue
@@ -5,41 +5,26 @@
         Términos y condiciones
       </h1>
       <div class="flex flex-col gap-4 text-sm md:text-base">
-        <p>
-          Senti Posadas, persona jurídica de derecho privado describe, a través de este documento, las normas de uso del sitio [sentiposadas.com.ar](http://sentiposadas.com.ar/) y de cualquier otro sitio web, tienda o aplicación operada por el titular.</p>
+        <p>Senti Posadas, persona jurídica de derecho privado describe, a través de este documento, las normas de uso del sitio <a href="https://sentiposadas.com.ar" target="_blank">https://sentiposadas.com.ar</a> y de cualquier otro sitio web, tienda o aplicación operada por el titular.</p>
+        <p>Al navegar por este sitio web, consideramos que está de acuerdo con las condiciones de uso que figuran a continuación.</p>
+        <p>Si no está de acuerdo con los términos de este acuerdo, le pedimos que no haga ningún otro uso de este sitio web, mucho menos que se registre o envíe sus datos personales.</p>
+        <p>Si cambiamos nuestras condiciones de uso, publicaremos el nuevo texto en este sitio web, con una fecha de revisión actualizada. Podemos modificar este documento en cualquier momento.</p>
+        <p>Si hay un cambio significativo en los términos de este acuerdo, podemos informarle utilizando la información de contacto que tenemos en nuestra base de datos o mediante una notificación.</p>
+        <p>El uso de este sitio web después de los cambios significa que usted acepta las condiciones de uso revisadas. Si, después de leer la versión revisada, no está de acuerdo con sus términos, por favor, termine su acceso.</p>
+        <h3>Usuario</h3>
+        <p>El uso de este sitio web le otorga automáticamente la condición de Usuario e implica su plena aceptación de todas las directrices y condiciones incluidas en estas Condiciones.</p>
+        <h3>Adhesión junto con la política de privacidad</h3>
+        <p>El uso de este sitio web implica la adhesión a estas Condiciones de Uso y a la versión más actualizada de la Política de Privacidad de Senti Posadas.</p>
+        <h3>Condiciones de acceso</h3>
+        <p>En general, el acceso al sitio web de Senti Posadas es gratuito y no requiere registro previo.</p>
+        <p>Sin embargo, para hacer uso de algunas funcionalidades, el usuario puede necesitar registrarse, creando una cuenta de usuario con un nombre de usuario y una contraseña de acceso.</p>
+        <p>Es responsabilidad del usuario proporcionar únicamente información correcta, auténtica, válida, completa y actualizada, así como no revelar su nombre de usuario y contraseña a terceros.</p>
+        <p>Algunas partes de este sitio web ofrecen al usuario la opción de publicar comentarios en determinadas áreas.Senti Posadas no consiente la publicación de contenidos de carácter discriminatorio, ofensivo o ilícito, o que infrinjan los derechos de autor o cualquier otro derecho de terceros.</p>
+        <p>La publicación de cualquier contenido por parte del usuario de este sitio web, incluidos los mensajes y comentarios, implica una licencia no exclusiva, irrevocable e irreversible para su uso, reproducción y publicación por parte de Senti Posadas en su sitio web, plataformas de internet y aplicaciones, o incluso en otras plataformas, sin ninguna restricción o limitación.</p>
 
-          Al navegar por este sitio web, consideramos que está de acuerdo con las condiciones de uso que figuran a continuación.
-
-          Si no está de acuerdo con los términos de este acuerdo, le pedimos que no haga ningún otro uso de este sitio web, mucho menos que se registre o envíe sus datos personales.
-
-          Si cambiamos nuestras condiciones de uso, publicaremos el nuevo texto en este sitio web, con una fecha de revisión actualizada. Podemos modificar este documento en cualquier momento.
-
-          Si hay un cambio significativo en los términos de este acuerdo, podemos informarle utilizando la información de contacto que tenemos en nuestra base de datos o mediante una notificación.
-
-          El uso de este sitio web después de los cambios significa que usted acepta las condiciones de uso revisadas. Si, después de leer la versión revisada, no está de acuerdo con sus términos, por favor, termine su acceso.
-
-          **Usuario**
-          El uso de este sitio web le otorga automáticamente la condición de Usuario e implica su plena aceptación de todas las directrices y condiciones incluidas en estas Condiciones.
-
-          **Adhesión junto con la política de privacidad**
-          El uso de este sitio web implica la adhesión a estas Condiciones de Uso y a la versión más actualizada de la Política de Privacidad de Senti Posadas.
-
-          **Condiciones de acceso**
-          En general, el acceso al sitio web de Senti Posadas es gratuito y no requiere registro previo.
-
-          Sin embargo, para hacer uso de algunas funcionalidades, el usuario puede necesitar registrarse, creando una cuenta de usuario con un nombre de usuario y una contraseña de acceso.
-
-          Es responsabilidad del usuario proporcionar únicamente información correcta, auténtica, válida, completa y actualizada, así como no revelar su nombre de usuario y contraseña a terceros.
-
-          Algunas partes de este sitio web ofrecen al usuario la opción de publicar comentarios en determinadas áreas.Senti Posadas no consiente la publicación de contenidos de carácter discriminatorio, ofensivo o ilícito, o que infrinjan los derechos de autor o cualquier otro derecho de terceros.
-
-          La publicación de cualquier contenido por parte del usuario de este sitio web, incluidos los mensajes y comentarios, implica una licencia no exclusiva, irrevocable e irreversible para su uso, reproducción y publicación por parte de Senti Posadas en su sitio web, plataformas de internet y aplicaciones, o incluso en otras plataformas, sin ninguna restricción o limitación.
-
-          **Plazos y modificaciones**
-          El funcionamiento de este sitio web es por tiempo indefinido.
-
-          La totalidad del sitio web o cada una de sus secciones podrá ser cerrada, suspendida o interrumpida unilateralmente por Senti Posadas, en cualquier momento y sin previo aviso.
-        </p>
+        <h3>Plazos y modificaciones</h3>
+        <p>El funcionamiento de este sitio web es por tiempo indefinido.</p>
+        <p>La totalidad del sitio web o cada una de sus secciones podrá ser cerrada, suspendida o interrumpida unilateralmente por Senti Posadas, en cualquier momento y sin previo aviso.</p>
       </div>
     </div>
   </section>
@@ -52,3 +37,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+a {
+  color: #35BC75;
+  text-decoration: underline;
+}
+h3 {
+  font-size: 24px;
+  font-weight: 600;
+}
+</style>
